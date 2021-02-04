c21fdb9052374fbfd9971c682c7a3869df0161f3
Jonathan Spencer <jonathanfspencer@gmail.com>
2021-02-03 21:25:07 -0600

    put female employment on x-axis

--- dat/week2.py
+++ dat/week2.py
@@ -32,23 +32,23 @@ data['polityscore'] = pandas.to_numeric(data['polityscore'])
 # I got weird results last week, so I want to further test the relationship between polity score and female employment
 # H0: There is no correlation between the percentage of women in a country who are employed and the polity score
 # H1: There is a correlation between the percentage of women in a country who are employed and the polity score
-# explanatory variable: polityscore
+# explanatory variable: femaleemployrate
 
 # create a categorical variable for whether most working age women are employed
 def howfememployed (row):
    if row['femaleemployrate'] > 75 :
-      return 3
+      return 4
    elif row['femaleemployrate'] > 50 :
-      return 2
+      return 3
    elif row['femaleemployrate'] > 25 :
-      return 1
+      return 2
    else :
-      return 0
+      return 1
          
 data['howfememployed'] = data.apply (lambda row: howfememployed (row),axis=1)
 # set new variable type to categorical
 data['howfememployed'] = pandas.to_numeric(data['howfememployed'])
-data['polityscore'] = data['polityscore'].astype('category')
+data['polityscore'] = pandas.to_numeric(data['polityscore'])
 
 
 #subset data to remove rows where any of the variables contain missing data
@@ -57,7 +57,7 @@ sub1=data.dropna(how='any', subset=['polityscore', 'howfememployed'])
 # Run a Chi-Square Test of Independence.
 
 # contingency table of observed counts
-ct1=pandas.crosstab(sub1['howfememployed'], sub1['polityscore'])
+ct1=pandas.crosstab(sub1['polityscore'], sub1['howfememployed'])
 print (ct1)
 
 # column percentages
@@ -70,18 +70,19 @@ print ('chi-square value, p value, expected counts')
 cs1= scipy.stats.chi2_contingency(ct1)
 print (cs1)
 
-# graph rate of female employment within each smoking polity score category
-polfemfig, polfemax = plt.subplots()
-seaborn.catplot(x="polityscore", y="howfememployed", data=sub1, kind="bar", ci=None)
-plt.xlabel('Polity Score (-10 to 10)')
-plt.ylabel('Rate of Female Employment (0 to 3)')
-plt.title('Rate of Female Employment and Polity Score')
+# graph polity score category within each rate of female employment
+seaborn.catplot(x="howfememployed", y="polityscore", data=sub1, kind="box", ci=None)
+plt.xlabel('Rate of Female Employment (1 to 4)')
+plt.ylabel('Polity Score (-10 to 10)')
+plt.title('Polity Score by Rate of Female Employment')
 plt.tight_layout()
-plt.savefig('dat/polfemfig.png')
+plt.savefig('dat/fempolfig.png')
 
 # You will need to analyze and interpret post hoc paired comparisons in instances where your 
 # original statistical test was significant, and you were examining more than two groups 
 # (i.e. more than two levels of a categorical, explanatory variable). 
+
+
 
 # Note: although it is possible to run large Chi-Square tables (e.g. 5 x 5, 4 x 6, etc.), 
 # the test is really only interpretable when your response variable has only 2 levels 
