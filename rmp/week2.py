0bcb936a7ecdd6f88f07c0212a58c7b95860e7b7
Jonathan Spencer <jonathanfspencer@gmail.com>
2021-02-16 12:40:25 -0600

    rmp week2 complete

--- rmp/week2.py
+++ rmp/week2.py
@@ -2,12 +2,42 @@
 """
 Jonathan Spencer
 Week 2 Assignment
+
+This week's assignment asks you to test a basic linear regression model for 
+the association between your primary explanatory variable and a response variable, 
+and to create a blog entry describing your results. 
+
+Data preparation for this assignment: 
+
+1) If your explanatory variable is categorical with more than two categories, 
+you will need to collapse it down to two categories, or subset your data to select 
+observations from 2 categories (next week you'll learn how to analyze categorical 
+explanatory variable with more than 2 categories). 
+
+2) If your response variable is categorical, you will need to identify a quantitative 
+variable in the data set that you can use as a response variable for this assignment. 
+Variables with response scales with 4-5 values that represent a change in magnitude 
+(for example, "strongly disagree to strongly agree", "never to often") can be 
+considered quantitative for the assignment.
+
+The assignment:
+
+1) If you have a categorical explanatory variable, make sure one of your 
+categories is coded "0" and generate a frequency table for this variable to 
+check your coding. If you have a quantitative explanatory variable, center it 
+so that the mean = 0 (or really close to 0) by subtracting the mean, and then 
+calculate the mean to check your centering. 
+
+2) Test a linear regression model and summarize the results in a couple of 
+sentences. Make sure to include statistical results (regression coefficients 
+and p-values) in your summary.
 """
 
-import pandas
 import numpy
+import pandas
+import statsmodels.api
+import statsmodels.formula.api as smf
 import seaborn
-import scipy
 import matplotlib.pyplot as plt
 
 #Set PANDAS to show all columns in DataFrame
@@ -22,49 +52,46 @@ data = pandas.read_csv('dat/gapminder.csv', low_memory=False)
 data = data.replace(r'^\s*$', numpy.NaN, regex=True)
 
 data['urbanrate'] = pandas.to_numeric(data['urbanrate'])
-data['oilperperson'] = pandas.to_numeric(data['oilperperson'])
+# data['oilperperson'] = pandas.to_numeric(data['oilperperson'])
 data['incomeperperson'] = pandas.to_numeric(data['incomeperperson'])
 
-# H0: There is no significant relationship between urban rate and oil use per person
-# H1: # H0: There is a significant relationship between urban rate and oil use per person
+# H0: There is no significant relationship between urban rate and income per person
+# H1: # H0: There is a significant relationship between urban rate and income per person
 # explanatory: urbanrate
-# response: oilperperson
+# response: incomeperperson
 # moderator: incomeperperson
 
 #subset data to remove rows where any of the variables contain missing data
 data=data.dropna()
 
+# Data Management
+# Use urban rate to create a two-category variable
+def urbhilow (row):
+    if row['urbanrate'] > 50:
+        return 1
+    else:
+        return 0
+
+data['urbhilow'] = data.apply(lambda row: urbhilow(row), axis=1)
+data['urbhilow'] = data['urbhilow'].astype('category')
+# Generate a frequency table for urbhilow
+urbhilowcount = data['urbhilow'].value_counts(sort=False)
+print('Urban Low and High Rate counts:')
+print(urbhilowcount)
+urbhilowpercent = data['urbhilow'].value_counts(sort=False, normalize=True)
+print('Urban Low and High Rate percentages:')
+print(urbhilowpercent)
+print()
+
 # Make a scatter plot to visualize the relationship
-fig1, scat1 = plt.subplots()
-scat1 = seaborn.regplot(x="urbanrate", y="oilperperson", fit_reg=True, data=data, ax=scat1)
-scat1.set_xlabel('2008 Urban Population Rate')
-scat1.set_ylabel('2010 Oil Consumption Per Capita')
-scat1.set_title('Urbanization Rate and Oil Consumption')
-fig1.savefig('dat/urbanoil.png')
+fig1, bar1 = plt.subplots()
+bar1 = seaborn.barplot(x="urbhilow", y="incomeperperson", data=data, ax=bar1)
+bar1.set_xlabel('2008 Urban Population Rate (0=Low, 1=High)')
+bar1.set_ylabel('2000 Income Per Capita in $USD')
+bar1.set_title('Urbanization Rate and Income Per Person')
+fig1.savefig('rmp/urbanincome.png')
 
-# Perform a Pearson Correlation Coefficient Test
-print ('Association between urban population rate and oil consumption')
-urbanoilp = scipy.stats.pearsonr(data['urbanrate'], data['oilperperson'])
-print(urbanoilp)
-if urbanoilp[1] < 0.05:
-    print('This relationship IS statistically significant')
-else:
-    print('This relationship is NOT statistically significant')
-print ()
-
-# See how incomeperperson works as a moderator
-# subset data to those countries at or below USD$2000 
-def incomegrp (row):
-   if row['incomeperperson'] < 2000:
-      return 1
-   else:
-      return 2
-   
-data['incomegrp'] = data.apply (lambda row: incomegrp (row),axis=1)
-print('Row counts above and below USD$2000:')
-chk1 = data['incomegrp'].value_counts(sort=False, dropna=False)
-print(chk1)
-# create a subframe for those countries below USD$2000
-sub2=data[(data['incomegrp'] == 1)]
-# create a subframe for those countries above USD$2000
-sub3=data[(data['incomegrp'] == 2)]
+# Perform an OLS Regression
+print ('Association between urban population rate and income per person')
+urbincomereg = smf.ols('incomeperperson ~ C(urbhilow)', data=data).fit()
+print(urbincomereg.summary())
